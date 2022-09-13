e47b8cb985816556f5bb786bf47958cb2b7f4c76
Farzon Lotfi <hi@farzon.org>
2022-09-12 22:07:55 -0400

    tests work locally. Need to test in github actions next

---
+++ scripts/run_cli.py
@@ -0,0 +1,31 @@
+import subprocess
+import json
+import sys
+import unittest
+
+class TestConfigJsonMethods(unittest.TestCase):
+    def test_config(self):
+        if(len(sys.argv)== 2):
+            with open("cli-test-cases/config.json", 'r') as f:
+                testConfigData = json.load(f)
+                for test in testConfigData:
+                    cliCommand = [test["execpath"]]
+                    cliCommand.extend(test["arguments"])
+                    process = subprocess.Popen(cliCommand, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+                    stdout, stderr = process.communicate()
+                    if(test.get("expected")):
+                        if type(test["expected"]) == int:
+                            self.assertEqual(int(stdout), test["expected"])
+                        if type(test["expected"]) == bool:
+                            self.assertEqual(bool(stdout), test["expected"])
+                        if type(test["expected"]) == str:
+                            self.assertEqual(stdout.decode("utf-8"), test["expected"])
+                    if(test.get("error")):
+                        self.assertEqual(stderr.decode("utf-8"), test["error"])
+                    if(test.get("comparePath")):
+                        with open(test["comparePath"]) as f:
+                            contents = f.read()
+                            self.assertEqual(stdout.decode("utf-8"), contents)
+
+if __name__ == '__main__':
+    unittest.main()
--- src/cli/main.cpp
+++ src/cli/main.cpp
@@ -6,11 +6,97 @@
 #include "Evaluator.h"
 #include "Symbol/SymbolTableMgr.h"
 #include "Syntax/SyntaxTree.h"
+#include <fstream>
+#include <functional>
 
-int main(int argc, char **argv) {
+struct Flags {
+  struct FlagName {
+    const std::string shortName;
+    const std::string name;
+  };
+  static const FlagName help;
+  static const FlagName eval;
+  static const FlagName repl;
+  static const FlagName showTree;
+  Flags() = delete;
+};
+
+class ParseFile {
+public:
+  ParseFile(const std::string &path,
+            std::function<void(std::string &, bool)> parseLineBehavior);
+  bool parse(bool showTree);
+
+private:
+  // TODO make parser hanle multiple files
+  // std::vector<std::string> mInputFilePaths;
+  std::string mInputFilePath;
+  std::function<void(std::string &, bool)> mParseLineBehavior;
+};
+
+ParseFile::ParseFile(const std::string &path,
+                     std::function<void(std::string &, bool)> parseLineBehavior)
+    : mInputFilePath(path), mParseLineBehavior(parseLineBehavior) {}
+
+bool ParseFile::parse(bool showTree) {
+  std::ifstream file(mInputFilePath);
+  if (!file.is_open()) {
+    // Error couldn't open the file
+    return false;
+  }
+
+  std::string line;
+  while (getline(file, line)) {
+    mParseLineBehavior(line, showTree);
+  }
+
+  file.close();
+  return file.eof() && !file.bad();
+}
+
+const Flags::FlagName Flags::help = {"-h", "--help"};
+const Flags::FlagName Flags::eval = {"-e", "--eval"};
+const Flags::FlagName Flags::repl = {"-r", "--repl"};
+const Flags::FlagName Flags::showTree = {"-s", "--show_tree"};
+
+void printUsage() {
+  std::cout << "usage: ./Warf \n";
+  std::cout << "       ./Warf <file_name>\n";
+  std::cout << "       ./Warf <file_name> --show_tree\n";
+  std::cout << "       ./Warf --repl\n";
+  std::cout << "       ./Warf --eval <string_to_evaluate>\n";
+  std::cout << "       ./Warf --eval <string_to_evaluate> --show_tree\n";
+  std::cout << "       ./Warf -h\n";
+  std::cout << "       ./Warf --help\n";
+}
+
+void evaluate(std::string &line, bool showTree) {
+  auto globalScope = SymbolTableMgr::getGlobalScope();
+  auto syntaxTree = SyntaxTree::Parse(line);
+  globalScope->GetTextSpan()->SetLength(line.size());
+  auto binder = std::make_unique<Binder>();
+  auto boundExpression = binder->BindExpression(syntaxTree->Root());
+
+  if (showTree) {
+    syntaxTree->PrintTree();
+  }
+
+  if (syntaxTree->Errors().empty() && binder->Errors().empty()) {
+    auto eval = std::make_unique<Evaluator>(std::move(boundExpression));
+    Value result = eval->Evaluate();
+    std::cout << result << std::endl;
+  } else {
+    for (auto error : syntaxTree->Errors()) {
+      std::cerr << error << std::endl;
+    }
+    for (auto error : binder->Errors()) {
+      std::cerr << error << std::endl;
+    }
+  }
+}
+
+void startRepl(bool showTree) {
   std::cout << "warfLang 1.0" << std::endl;
-  bool showTree = false;
-  SymbolTableMgr::init();
   while (true) {
     try {
       std::string line = "";
@@ -27,28 +113,7 @@ int main(int argc, char **argv) {
         exit(0);
       }
 
-      auto globalScope = SymbolTableMgr::getGlobalScope();
-      auto syntaxTree = SyntaxTree::Parse(line);
-      globalScope->GetTextSpan()->SetLength(line.size());
-      auto binder = std::make_unique<Binder>();
-      auto boundExpression = binder->BindExpression(syntaxTree->Root());
-
-      if (showTree) {
-        syntaxTree->PrintTree();
-      }
-
-      if (syntaxTree->Errors().empty() && binder->Errors().empty()) {
-        auto eval = std::make_unique<Evaluator>(std::move(boundExpression));
-        Value result = eval->Evaluate();
-        std::cout << result << std::endl;
-      } else {
-        for (auto error : syntaxTree->Errors()) {
-          std::cerr << error << std::endl;
-        }
-        for (auto error : binder->Errors()) {
-          std::cerr << error << std::endl;
-        }
-      }
+      evaluate(line, showTree);
 
     } catch (std::runtime_error &error) {
       std::cerr << error.what() << std::endl;
@@ -58,4 +123,57 @@ int main(int argc, char **argv) {
       std::cin.get();
     }
   }
+}
+
+int main(int argc, char **argv) {
+  SymbolTableMgr::init();
+  bool isEval = false;
+  bool isRepl = false;
+  bool showTree = false;
+  int showTreeIndex = 0;
+  std::string evalStr;
+  for (int i = 1; i < argc; i++) {
+    if (Flags::help.shortName == argv[i] || Flags::help.name == argv[i]) {
+      printUsage();
+      return 0;
+    }
+    if (Flags::eval.shortName == argv[i] || Flags::eval.name == argv[i]) {
+      isEval = true;
+      if(i+1 >= argc) {
+        std::cerr << "Expected eval argument string" << std::endl;
+        return -1;
+      }
+      evalStr = std::string(argv[i + 1]);
+    } else if (Flags::showTree.shortName == argv[i] ||
+               Flags::showTree.name == argv[i]) {
+      showTree = true;
+      showTreeIndex = i;
+    } else if (Flags::repl.shortName == argv[i] ||
+               Flags::repl.name == argv[i]) {
+      isRepl = true;
+    } else if(argv[i][0] == '-') {
+      std::cerr << "Invalid flag: " << argv[i] << std::endl;
+      printUsage();
+      return 0;
+    }
+  }
+
+  if (isEval) {
+    evaluate(evalStr, showTree);
+    return 0;
+  }
+  if (isRepl || argc == 1 || (showTree && argc == 2)) {
+    startRepl(showTree);
+    return 0;
+  }
+  if (argc > 3) {
+    printUsage();
+    return 0;
+  }
+
+  int filePathIndex = 1 == showTreeIndex ? 2 : 1;
+  std::string filePath(argv[filePathIndex]);
+  ParseFile file(filePath, evaluate);
+  file.parse(showTree);
+  return 0;
 }
