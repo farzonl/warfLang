7b3dea3e6c3bbaf7f208dd26ad1158dad0caa95c
Farzon Lotfi <hi@farzon.org>
2022-10-04 23:58:43 -0400

    This change makes it so that windows PE executable unit tests will now build on Linux. The fix was to invoke XClang via add_compile_options. The winsdk_vfs_overlay.yaml file is finally propogating. As such this change fixes #29.

--- cmake_config/llvm-win-cross.cmake
+++ cmake_config/llvm-win-cross.cmake
@@ -27,6 +27,7 @@ set(WIN_SDK_PATH  "win_sysroot/WINSDK/${WIN_SDK_VERSION}")
 set(WIN_MSVC_PATH "win_sysroot/MSVC/${MSVC_VERSION}")
 set(WIN_SDK_INCLUDE_PATH "${WIN_SDK_PATH}/include")
 set(WIN_SDK_LIB_PATH "${WIN_SDK_PATH}/lib")
+set(WiN_VFS_OVERLAY ${CMAKE_CURRENT_LIST_DIR}/../packages/winsdk_vfs_overlay.yaml)
 
 if(APPLE)
     include_directories(/usr/local/opt/llvm/include)
@@ -46,11 +47,32 @@ link_directories(
                  ${CMAKE_CURRENT_LIST_DIR}/../packages/${WIN_SDK_LIB_PATH}/um/x64
                  ${CMAKE_CURRENT_LIST_DIR}/../packages/${WIN_SDK_LIB_PATH}/ucrt/x64)
 
-set(GNU_UNDEF_FLAGS "-U__GNUC__ -U__gnu_linux__ -U__GNUC_MINOR__ -U__GNUC_PATCHLEVEL__ -U__GNUC_STDC_INLINE__")
-set(XCLANG_FLAGS "-Xclang -ivfsoverlay -Xclang ${CMAKE_CURRENT_LIST_DIR}/../packages/winsdk_vfs_overlay.yaml -Xclang -disable-llvm-verifier -Xclang '--dependent-lib=msvcrt' -Xclang '--dependent-lib=ucrt' -Xclang '--dependent-lib=oldnames' -Xclang '--dependent-lib=vcruntime'")
+add_compile_options(
+    # XCLANG_FLAGS
+    "SHELL:-Xclang -ivfsoverlay -Xclang ${WiN_VFS_OVERLAY}"
+    "SHELL:-Xclang --dependent-lib=oldnames"
+    "SHELL:-Xclang --dependent-lib=ucrt"
+    "SHELL:-Xclang --dependent-lib=vcruntime"
+    "SHELL:-Xclang --dependent-lib=msvcrt$<$<CONFIG:Debug>:d>"
+    "SHELL:-Xclang -disable-llvm-verifier"
+    # WARNINGS
+    -Wno-msvc-not-found
+    # GNU undefs
+    "-U__GNUC__"
+    "-U__gnu_linux__" 
+    "-U__GNUC_MINOR__" 
+    "-U__GNUC_PATCHLEVEL__" 
+    "-U__GNUC_STDC_INLINE__"
+    #Definitions
+    "-DWIN32" 
+    "-D_WIN#@"
+    "-D_MT" 
+    "-D_DLL" 
+    "-D_CRT_SECURE_NO_WARNINGS" 
+    "-D_CRT_NONSTDC_NO_DEPRECATE"
+)
+
 set(F_FLAGS "-fms-compatibility-version=19.11 -fms-extensions -fdelayed-template-parsing -fexceptions -mthread-model posix -fno-threadsafe-statics")
-set(DEF_FLAGS  "-DWIN32 -D_WIN#@ -D_MT -D_DLL -D_CRT_SECURE_NO_WARNINGS -D_CRT_NONSTDC_NO_DEPRECATE")
-set(WARN_FLAGS "-Wno-msvc-not-found")
-set(COMPILE_FLAGS "--target=${TARGET_TRIPLE} -nostdlib -lmsvcrt ${F_FLAGS} ${DEF_FLAGS} ${XCLANG_FLAGS} ${GNU_UNDEF_FLAGS} ${WARN_FLAGS}" )
+set(COMPILE_FLAGS "--target=${TARGET_TRIPLE} -nostdlib -lmsvcrt ${F_FLAGS}" )
 set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS}  ${COMPILE_FLAG}" CACHE STRING "" FORCE)
 set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} ${COMPILE_FLAG}" CACHE STRING "" FORCE)
