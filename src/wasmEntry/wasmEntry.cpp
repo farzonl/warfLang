e253e3ebba710bfc161398d6d75549330c690ec6
Farzon Lotfi <farzonl@icloud.com>
2023-04-30 14:12:11 -0400

    create a custom web frontend for Warf

---
+++ src/wasmEntry/wasmEntry.cpp
@@ -0,0 +1,69 @@
+#include <string>
+#include <sstream>
+#include <iostream>
+#include <stdbool.h>
+
+#include <emscripten.h>
+
+#include "Binding/Binder.h"
+#include "Evaluator.h"
+#include "Symbol/SymbolTableMgr.h"
+#include "Syntax/SyntaxTree.h"
+
+extern "C" {
+ EMSCRIPTEN_KEEPALIVE void InitWarf();
+ EMSCRIPTEN_KEEPALIVE char* RunWarf(char* input, bool showSyntaxTree);
+ EMSCRIPTEN_KEEPALIVE char* ShowSyntaxTree();
+}
+
+char* g_ReturnBuffer = nullptr;
+char* g_showSyntaxTree = nullptr;
+
+//EMSCRIPTEN_KEEPALIVE
+void InitWarf() {
+    SymbolTableMgr::init();
+}
+
+//EMSCRIPTEN_KEEPALIVE
+char* ShowSyntaxTree() {
+  return g_showSyntaxTree;
+}
+
+//EMSCRIPTEN_KEEPALIVE
+char* RunWarf(char* input, bool showSyntaxTree) {
+  
+  std::string s(input);
+  
+  if(g_ReturnBuffer) {
+    delete g_ReturnBuffer;
+    g_ReturnBuffer = nullptr;
+  }
+
+  if(g_showSyntaxTree && showSyntaxTree) {
+    delete g_showSyntaxTree;
+    g_showSyntaxTree = nullptr;
+  }
+
+
+  auto syntaxTree = SyntaxTree::Parse(s);
+  auto binder = std::make_unique<Binder>();
+  auto boundExpression = binder->BindExpression(syntaxTree->Root());
+  auto eval = std::make_unique<Evaluator>(std::move(boundExpression));
+  
+  std::stringstream outputStream;
+  outputStream << eval->Evaluate() << std::endl;
+  std::string outputStr = outputStream.str();
+  g_ReturnBuffer = new char(outputStr.size());
+  stpncpy(g_ReturnBuffer, outputStr.c_str(), outputStr.size());
+
+  if (showSyntaxTree) {
+    std::stringstream syntaxTreeStream;
+    syntaxTree->PrintTree(syntaxTreeStream);
+
+    std::string outputSyntaxTree = syntaxTreeStream.str();
+    g_showSyntaxTree = new char(outputSyntaxTree.size());
+    stpncpy(g_showSyntaxTree, outputSyntaxTree.c_str(), outputSyntaxTree.size());
+  }
+  
+  return g_ReturnBuffer;
+}
