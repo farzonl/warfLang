ebd82594c15120effe554b0887edabf401a6730e
Farzon Lotfi <farzon@farzon.org>
2023-09-25 02:29:54 -0400

    confirm theory that commit from 5 months ago will work

--- scripts/osxcrossFetch.sh
+++ scripts/osxcrossFetch.sh
@@ -9,5 +9,8 @@ SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 if [ ! -e $SCRIPT_DIR/../packages/osxcross ]; then
     pushd $SCRIPT_DIR/../packages/
     git clone https://github.com/tpoechtrager/osxcross
+    pushd osxcross
+    git checkout 26063b07e2aa6a32ba4be31933fd7bda45886d22
+    popd
     popd
 fi
