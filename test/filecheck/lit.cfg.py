c0f0ab9679345a67c66d1205ef3fcc0edb226fc1
Farzon Lotfi <hi@farzon.org>
2026-09-19 16:57:49 -0400

    add filecheck support

---
+++ test/filecheck/lit.cfg.py
@@ -0,0 +1,14 @@
+import os
+
+import lit.formats
+
+config.name = "warf"
+config.test_format = lit.formats.ShTest()
+config.suffixes = [".wf"]
+config.excludes = ["Inputs"]
+config.test_source_root = os.path.dirname(__file__)
+config.test_exec_root = config.test_source_root
+
+site_config = os.environ.get("WARF_LIT_SITE_CONFIG")
+if site_config:
+    lit_config.load_config(config, site_config)
